7491f386224ff168b5d7dfb90565953c2789c99b
xukiki <20599378@qq.com>
2017-05-25 19:05:40 +0800

    1.0.42

--- QqcRequest/QqcDataModel.h
+++ QqcRequest/QqcDataModel.h
@@ -7,6 +7,7 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "QqcBaseModel.h"
 
 @interface QqcDataModel : QqcBaseModel
 
--- QqcRequest/QqcParamModel.h
+++ QqcRequest/QqcParamModel.h
@@ -7,6 +7,7 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "QqcBaseModel.h"
 
 @interface QqcParamModel : QqcBaseModel
 
